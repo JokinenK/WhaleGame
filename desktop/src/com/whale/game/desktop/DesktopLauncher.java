aa900b6c394d401319dc812daaf39e37d50f4052
olli1337 <olli@havilehto.fi>
2015-12-04 20:42:36 +0200

    Initial commit

---
+++ desktop/src/com/whale/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.whale.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.whale.game.WhaleGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WhaleGame(), config);
+	}
+}
