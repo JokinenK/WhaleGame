7d78e16370fbc8e76a5ae68452b25d98f9eec685
olli1337 <olli@havilehto.fi>
2015-12-04 22:02:42 +0200

    MainMenuScreen and GameScreen implemented.

---
+++ core/src/com/whale/game/GameScreen.java
@@ -0,0 +1,61 @@
+package com.whale.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+
+/**
+ * Created by Olli Havilehto on 4.12.2015.
+ */
+public class GameScreen implements Screen {
+
+    WhaleGame game;
+    Texture img;
+
+    public GameScreen(WhaleGame gam) {
+        this.game = gam;
+        img = new Texture("badlogic.jpg");
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        game.batch.begin();
+        game.batch.draw(img,0,0);
+        game.batch.end();
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ core/src/com/whale/game/MainMenuScreen.java
@@ -0,0 +1,77 @@
+package com.whale.game;
+
+/**
+ * Created by Olli Havilehto on 4.12.2015.
+ */
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+
+public class MainMenuScreen implements Screen {
+
+    OrthographicCamera camera;
+    WhaleGame game;
+    Texture img;
+
+    public MainMenuScreen(WhaleGame gam) {
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+        this.game = gam;
+        img = new Texture("badlogic.jpg");
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+
+        game.batch.begin();
+        game.font.draw(game.batch, "Welcome to Drop!!! ", 100, 150);
+        game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
+        game.batch.end();
+
+        if (Gdx.input.isTouched()) {
+            game.setScreen(new GameScreen(game));
+            dispose();
+        }
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    //...Rest of class omitted for succinctness.
+}
